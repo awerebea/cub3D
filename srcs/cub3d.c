c5b76eee99bbbc85ae1259d278bdafc72c6c9014
awerebea <awerebea.21@gmail.com>
2020-07-18 21:11:12 +0300

    intermediate commit

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/18 13:47:17 by awerebea          #+#    #+#             */
-/*   Updated: 2020/07/18 17:42:59 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/07/18 21:10:24 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,46 +20,43 @@ void	f_sdf_init(t_sdf *sdf)
 	sdf->screenshot = 0;
 }
 
-void	f_print_error_1_10(int errcode)
+void	f_print_error_1_3(int errcode)
 {
 	if (errcode == 1)
-		ft_putstr_fd("Error 1: no map file specified\n", 1);
+		ft_putstr_fd("ERROR 1: wrong number of arguments.\n\
+DESCRIPTION: There can be only one or two arguments. The first argument MUST \
+be a '.cub' file wih map.\nThe second argument CAN be '--save', \
+in this case the 'cub3D' just takes screenshot of spawn scene and quits.\n", 2);
 	else if (errcode == 2)
-		ft_putstr_fd("Error 2: map file must have name (at least 1 symbol) and \
-extension '.cub'\n", 1);
+		ft_putstr_fd("ERROR 2: wrong name of map file.\n\
+DESCRIPTION: the first argument is always the name of the map file, and it \
+must have a name with at least one character and an extension '.cub'.\n", 2);
 	else if (errcode == 3)
-		ft_putstr_fd("Error 3: wrong extension of map file, \
-it must be '.cub'\n", 1);
-	else if (errcode == 4)
-		ft_putstr_fd("Error 4: wrong second argument, \
-it can be only '--save' (if You want take screenshot of scene and quit)\n", 1);
-	else if (errcode == 5)
-		ft_putstr_fd("Error 5: wrong number of arguments, \
-it can be minimum one (.cub file) and maximum two (if You want take screenshot \
-of scene and quit)\n", 1);
+		ft_putstr_fd("ERROR 3: wrong second argument.\n\
+DESCRIPTION: it can be only '--save', in this case the 'cub3D' just takes \
+screenshot of spawn scene and quits.\n", 2);
 }
 
 int		f_exit(int errcode)
 {
 	if (errcode > 0 && errcode <= 10)
-		f_print_error_1_10(errcode);
+		f_print_error_1_3(errcode);
 	else
 		ft_putstr_fd(strerror(errno), 1);
-	return (0);
+	return (errcode);
 }
 
 int		f_check_args(int argc, char **argv, t_sdf *sdf)
 {
-	if (argc < 2)
+	if (argc < 2 || argc > 3)
 		return (1);
-	if (ft_strlen(argv[1]) < 5)
+	if (ft_strlen(argv[1]) < 5 \
+			|| (ft_strncmp(argv[1] + ft_strlen(argv[1]) - 4, ".cub", 5)))
 		return (2);
-	if ((ft_strncmp(argv[1] + ft_strlen(argv[1]) - 4, ".cub", 5)))
-		return (3);
 	if (argc == 3)
 	{
 		if ((ft_strncmp(argv[2], "--save", 6)))
-			return (4);
+			return (3);
 		else
 			sdf->screenshot = 1;
 	}
