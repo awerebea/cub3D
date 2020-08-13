8ab0ceeee08807245a11850bca06f43a6f9c5394
awerebea <awerebea.21@gmail.com>
2020-08-13 20:35:38 +0300

    new sprites swap

--- srcs/engine/sprites_handling.c
+++ srcs/engine/sprites_handling.c
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 00:10:47 by awerebea          #+#    #+#             */
-/*   Updated: 2020/08/13 19:48:01 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/08/13 20:35:29 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,38 +15,45 @@
 
 void		f_sprites_swap_with_next(t_sp *sp)
 {
-	t_sp	*tmp_n;
-	t_sp	*tmp_n_n;
-	t_sp	*tmp_p;
+	float	tmp_data;;
 
-	tmp_n = sp->next;
-	tmp_n_n = sp->next->next;
-	tmp_p = sp->prev;
-	sp->next = sp;
-	sp = tmp_n;
-	tmp_n = sp->prev;
-	sp->next = tmp_n;
-	sp->prev = tmp_p;
-	sp->next->prev = sp;
-	sp->next->next = tmp_n_n;
+	tmp_data = (float)sp->next->id;
+	sp->next->id = sp->id;
+	sp->id = (int)tmp_data;
+	tmp_data = sp->next->x;
+	sp->next->x = sp->x;
+	sp->x = tmp_data;
+	tmp_data = sp->next->y;
+	sp->next->y = sp->y;
+	sp->y = tmp_data;
+	tmp_data = sp->next->dist;
+	sp->next->dist = sp->dist;
+	sp->dist = tmp_data;
 }
 
 void		f_sprites_list_sort_by_dist_desc(t_mlx *mlx)
 {
 	t_sp	*sp;
+	int		swap_flag;
 
 	sp = mlx->sp_list;
-	/* while (sp) */
-	/* { */
-		/* while (sp->next) */
-		/* { */
-			/* if (sp->dist < sp->next->dist) */
-				/* f_sprites_swap_with_next(sp); */
-		/* } */
-		/* sp = sp->next; */
-	/* } */
-	if (sp->next)
-		f_sprites_swap_with_next(sp);
+	swap_flag = 1;
+	while (swap_flag)
+	{
+		swap_flag = 0;
+		while (sp->next)
+		{
+			if (sp->dist < sp->next->dist)
+			{
+				f_sprites_swap_with_next(sp);
+				swap_flag = 1;
+			}
+			sp = sp->next;
+		}
+		sp = mlx->sp_list;
+	}
+	/* if (sp->next)                     */
+	/*     f_sprites_swap_with_next(sp); */
 }
 
 void		f_sprites_dist_calculating(t_mlx *mlx)
