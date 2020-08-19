0fa9a3ba0aab7ad91aa42ac7c7f36ecdb157263f
awerebea <awerebea.21@gmail.com>
2020-08-20 00:30:22 +0300

    code cleaning (prototypes of few unused functions removed)

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/18 14:12:20 by awerebea          #+#    #+#             */
-/*   Updated: 2020/08/18 16:47:05 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/08/20 00:17:54 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,39 +132,36 @@ typedef struct	s_mlx
 }				t_mlx;
 
 int				f_check_args(int argc, char **argv, t_sdf *opts);
-int				f_check_textures_for_valid(t_mlx *mlx);
-int				f_close_n_exit(t_mlx *mlx);
+void			f_skip_spaces(char *line, int *i);
 int				f_cub3d_atoi(char *line, int *i);
-int				f_draw_all(t_mlx *mlx);
-int				f_exit(int errcode, t_sdf *opts);
-int				f_game_init(t_mlx *mlx);
 int				f_get_a_from_int(int argb);
-int				f_get_b_from_int(int argb);
-int				f_get_g_from_int(int argb);
-int				f_get_int_from_argb(int a, int r, int g, int b);
 int				f_get_r_from_int(int argb);
+int				f_get_g_from_int(int argb);
+int				f_get_b_from_int(int argb);
+int				f_get_int_from_argb(int a, int r, int g, int b);
+int				f_close_n_exit(t_mlx *mlx);
+int				f_exit(int errcode, t_sdf *opts);
+void			f_print_err(int errcode, t_sdf *opts);
+void			f_print_err_2(int errcode, t_sdf *opts);
+int				f_pars_desc_file(char *map_file, t_sdf *opts);
+int				f_pars_map(char *line, t_sdf *opts);
+int				f_map_array_preparing(t_sdf *opts);
+int				f_pars_sprite_texture(char *line, int i, t_sdf *opts);
+int				f_pars_wall_textures(char *line, int i, t_sdf *opts);
+void			f_game(t_sdf *opts);
+int				f_game_init(t_mlx *mlx);
 int				f_key_press(int key, t_mlx *mlx);
 int				f_key_process(t_mlx *mlx);
 int				f_key_release(int key, t_mlx *mlx);
-int				f_map_array_preparing(t_sdf *opts);
-int				f_pars_ceiling_color(char *line, int i, t_sdf *opts);
-int				f_pars_desc_file(char *map_file, t_sdf *opts);
-int				f_pars_floor_color(char *line, int i, t_sdf *opts);
-int				f_pars_map(char *line, t_sdf *opts);
-int				f_pars_sprite_texture(char *line, int i, t_sdf *opts);
-int				f_pars_wall_textures(char *line, int i, t_sdf *opts);
-int				f_sprites_handling(t_mlx *mlx);
-void			f_draw_background(t_mlx *mlx);
-void			f_draw_sprite(t_mlx *mlx, t_sp *sp);
-void			f_error_mlx(int errcode, t_sdf *opts);
-void			f_game(t_sdf *opts);
-void			f_player_pos_init(t_mlx *mlx);
-void			f_print_err(int errcode, t_sdf *opts);
-void			f_print_err_2(int errcode, t_sdf *opts);
 void			f_raycasting(t_mlx *mlx);
-void			f_screenshot(t_mlx *mlx);
-void			f_skip_spaces(char *line, int *i);
+int				f_sprites_handling(t_mlx *mlx);
 void			f_sprites_init(t_mlx *mlx);
+int				f_draw_all(t_mlx *mlx);
+void			f_draw_sprite(t_mlx *mlx, t_sp *sp);
+void			f_screenshot(t_mlx *mlx);
 void			my_mlx_pixel_put(t_img *img, int x, int y, int color);
+int				f_pars_floor_color(char *line, int i, t_sdf *opts);
+int				f_pars_ceiling_color(char *line, int i, t_sdf *opts);
+void			f_draw_background(t_mlx *mlx);
 
 #endif
--- includes/cub3d_bonus.h
+++ includes/cub3d_bonus.h
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/18 14:12:20 by awerebea          #+#    #+#             */
-/*   Updated: 2020/08/18 16:52:52 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/08/20 00:17:48 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -155,44 +155,41 @@ typedef struct	s_mlx
 	int			errcode;
 }				t_mlx;
 
-int				f_add_shade(int color, float shade);
 int				f_check_args(int argc, char **argv, t_sdf *opts);
-int				f_check_textures_for_valid(t_mlx *mlx);
-int				f_close_n_exit(t_mlx *mlx);
+void			f_skip_spaces(char *line, int *i);
 int				f_cub3d_atoi(char *line, int *i);
-int				f_draw_all(t_mlx *mlx);
-int				f_exit(int errcode, t_sdf *opts);
-int				f_game_init(t_mlx *mlx);
 int				f_get_a_from_int(int argb);
-int				f_get_b_from_int(int argb);
-int				f_get_g_from_int(int argb);
-int				f_get_int_from_argb(int a, int r, int g, int b);
 int				f_get_r_from_int(int argb);
+int				f_get_g_from_int(int argb);
+int				f_get_b_from_int(int argb);
+int				f_get_int_from_argb(int a, int r, int g, int b);
+int				f_close_n_exit(t_mlx *mlx);
+int				f_exit(int errcode, t_sdf *opts);
+void			f_print_err(int errcode, t_sdf *opts);
+void			f_print_err_2(int errcode, t_sdf *opts);
+int				f_pars_desc_file(char *map_file, t_sdf *opts);
+int				f_pars_map(char *line, t_sdf *opts);
+int				f_map_array_preparing(t_sdf *opts);
+int				f_pars_sprite_texture(char *line, int i, t_sdf *opts);
+int				f_pars_wall_textures(char *line, int i, t_sdf *opts);
+void			f_game(t_sdf *opts);
+int				f_game_init(t_mlx *mlx);
 int				f_key_press(int key, t_mlx *mlx);
 int				f_key_process(t_mlx *mlx);
 int				f_key_release(int key, t_mlx *mlx);
-int				f_map_array_preparing(t_sdf *opts);
-int				f_pars_ceiling_texture(char *line, int i, t_sdf *opts);
-int				f_pars_desc_file(char *map_file, t_sdf *opts);
-int				f_pars_floor_texture(char *line, int i, t_sdf *opts);
-int				f_pars_map(char *line, t_sdf *opts);
-int				f_pars_sprite_texture(char *line, int i, t_sdf *opts);
-int				f_pars_wall_textures(char *line, int i, t_sdf *opts);
+void			f_raycasting(t_mlx *mlx);
 int				f_sprites_handling(t_mlx *mlx);
-void			f_draw_minimap(t_mlx *mlx);
+void			f_sprites_init(t_mlx *mlx);
+int				f_draw_all(t_mlx *mlx);
 void			f_draw_sprite(t_mlx *mlx, t_sp *sp);
+void			f_screenshot(t_mlx *mlx);
+void			my_mlx_pixel_put(t_img *img, int x, int y, int color);
+int				f_pars_floor_texture(char *line, int i, t_sdf *opts);
+int				f_pars_ceiling_texture(char *line, int i, t_sdf *opts);
 void			f_draw_textured_background(t_mlx *mlx);
-void			f_error_mlx(int errcode, t_sdf *opts);
-void			f_game(t_sdf *opts);
 void			f_minimap_init(t_mlx *mlx);
 void			f_player_pos_init(t_mlx *mlx);
-void			f_player_pos_init(t_mlx *mlx);
-void			f_print_err(int errcode, t_sdf *opts);
-void			f_print_err_2(int errcode, t_sdf *opts);
-void			f_raycasting(t_mlx *mlx);
-void			f_screenshot(t_mlx *mlx);
-void			f_skip_spaces(char *line, int *i);
-void			f_sprites_init(t_mlx *mlx);
-void			my_mlx_pixel_put(t_img *img, int x, int y, int color);
+void			f_draw_minimap(t_mlx *mlx);
+int				f_add_shade(int color, float shade);
 
 #endif
