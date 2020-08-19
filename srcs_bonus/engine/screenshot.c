22e82b6c151523fb4434675e2b569f2e819c0ad5
awerebea <awerebea.21@gmail.com>
2020-08-20 00:29:41 +0300

    some code optimisations

--- srcs_bonus/engine/screenshot.c
+++ srcs_bonus/engine/screenshot.c
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 22:12:13 by awerebea          #+#    #+#             */
-/*   Updated: 2020/08/18 21:32:19 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/08/19 23:55:56 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,48 +45,8 @@ static char	*f_get_filename(t_mlx *mlx)
 	return (filename);
 }
 
-static char	*f_bmp_file_header_generate(t_mlx *mlx)
-{
-	char	*file_header;
-	int		file_size;
-
-	if (!(file_header = malloc(sizeof(char) * 14)))
-		return (NULL);
-	ft_bzero(file_header, 14);
-	file_size = 14 + 40 + mlx->x_win_size * mlx->y_win_size * \
-			mlx->img.bits_per_pix / 8;
-	file_header[0] = 'B';
-	file_header[1] = 'M';
-	file_header[2] = (char)(file_size);
-	file_header[3] = (char)(file_size >> 8);
-	file_header[4] = (char)(file_size >> 16);
-	file_header[5] = (char)(file_size >> 24);
-	file_header[10] = (char)(14 + 40);
-	return (file_header);
-}
-
-static char	*f_bmp_info_header_generate(t_mlx *mlx)
-{
-	char	*info_header;
-
-	if (!(info_header = malloc(sizeof(char) * 40)))
-		return (NULL);
-	ft_bzero(info_header, 40);
-	info_header[0] = (char)(40);
-	info_header[4] = (char)(mlx->x_win_size);
-	info_header[5] = (char)(mlx->x_win_size >> 8);
-	info_header[6] = (char)(mlx->x_win_size >> 16);
-	info_header[7] = (char)(mlx->x_win_size >> 24);
-	info_header[8] = (char)(-mlx->y_win_size);
-	info_header[9] = (char)(-mlx->y_win_size >> 8);
-	info_header[10] = (char)(-mlx->y_win_size >> 16);
-	info_header[11] = (char)(-mlx->y_win_size >> 24);
-	info_header[12] = (char)(1);
-	info_header[14] = (char)(mlx->img.bits_per_pix);
-	return (info_header);
-}
-
-static int	f_bmp_image_write(char *file_header, char *info_header, t_mlx *mlx)
+static int	f_bmp_image_write(t_mlx *mlx, char *bitmap_file_header, \
+															char *dib_header)
 {
 	int		fd;
 	char	*filename;
@@ -95,36 +55,52 @@ static int	f_bmp_image_write(char *file_header, char *info_header, t_mlx *mlx)
 		return (mlx->errcode);
 	if ((fd = open(filename, O_RDWR | O_CREAT, 664)) < 0)
 		return (fd);
-	write(fd, file_header, 14);
-	write(fd, info_header, 40);
+	write(fd, bitmap_file_header, 14);
+	write(fd, dib_header, 40);
 	write(fd, mlx->img.addr, mlx->x_win_size * mlx->y_win_size * \
 			mlx->img.bits_per_pix / 8);
-	free(file_header);
-	free(info_header);
 	free(filename);
 	return (close(fd) < 0) ? 320 : 0;
 }
 
+static void	f_bmp_headers_generate(t_mlx *mlx, char *bitmap_file_header, \
+															char *dib_header)
+{
+	int		file_size;
+
+	file_size = 14 + 40 + mlx->x_win_size * mlx->y_win_size * \
+			mlx->img.bits_per_pix / 8;
+	bitmap_file_header[0] = 'B';
+	bitmap_file_header[1] = 'M';
+	bitmap_file_header[2] = (char)(file_size);
+	bitmap_file_header[3] = (char)(file_size >> 8);
+	bitmap_file_header[4] = (char)(file_size >> 16);
+	bitmap_file_header[5] = (char)(file_size >> 24);
+	bitmap_file_header[10] = (char)(14 + 40);
+	dib_header[0] = (char)(40);
+	dib_header[4] = (char)(mlx->x_win_size);
+	dib_header[5] = (char)(mlx->x_win_size >> 8);
+	dib_header[6] = (char)(mlx->x_win_size >> 16);
+	dib_header[7] = (char)(mlx->x_win_size >> 24);
+	dib_header[8] = (char)(-mlx->y_win_size);
+	dib_header[9] = (char)(-mlx->y_win_size >> 8);
+	dib_header[10] = (char)(-mlx->y_win_size >> 16);
+	dib_header[11] = (char)(-mlx->y_win_size >> 24);
+	dib_header[12] = (char)(1);
+	dib_header[14] = (char)(mlx->img.bits_per_pix);
+}
+
 void		f_screenshot(t_mlx *mlx)
 {
-	char	*file_header;
-	char	*info_header;
+	char	bitmap_file_header[14];
+	char	dib_header[40];
 
-	file_header = NULL;
-	info_header = NULL;
+	ft_bzero(bitmap_file_header, 14);
+	ft_bzero(dib_header, 40);
 	f_draw_textured_background(mlx);
 	f_raycasting(mlx);
 	f_draw_minimap(mlx);
-	if (!(file_header = f_bmp_file_header_generate(mlx)) || \
-		!(info_header = f_bmp_info_header_generate(mlx)))
-	{
-		mlx->errcode = 200;
-		if (file_header)
-			free(file_header);
-		if (info_header)
-			free(info_header);
-		f_close_n_exit(mlx);
-	}
-	mlx->errcode = f_bmp_image_write(file_header, info_header, mlx);
+	f_bmp_headers_generate(mlx, bitmap_file_header, dib_header);
+	mlx->errcode = f_bmp_image_write(mlx, bitmap_file_header, dib_header);
 	f_close_n_exit(mlx);
 }
